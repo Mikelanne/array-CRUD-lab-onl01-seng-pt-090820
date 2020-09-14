af323265cc3e717a0437102ade1ea2ea1341c1eb
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-14 21:20:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+   twin_peaks = ["Wow", "Audrey", "Gordon", "Norma"]
+   first_word = twin_peaks.shift
 end
 
 def retrieve_element_from_index(array, index_number)
